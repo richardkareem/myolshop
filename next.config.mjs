028a94964b11f6807fded9911f5872ab6cd0e51e
Richard Abdul Kareem <risyadkarim26@gmail.com>
2024-07-03 00:56:42 +0700

    tes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     images: {
         domains: ['spaces.ilfen.co', 'images.tokopedia.net']
-    }
+    },
+    experimental: {
+        workerThreads: false,
+        cpus: 1,
+    },
 };
 
 export default nextConfig;
--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -1,8 +1,5 @@
-import getDetailProduct from 'component/utils/getDetailProduct'
-import React, {  } from 'react'
+import React from 'react'
 const DetailPage = async() => {
-  const data = await getDetailProduct('wewew');
-  console.log(data)
   return (
     <div>DetailPage</div>
   )
